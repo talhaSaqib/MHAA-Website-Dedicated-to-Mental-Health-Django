4b1834550dc2d8fd75ce36074bec06bf0829849d
Talha Saqib <43103740+talhaSaqib@users.noreply.github.com>
2020-06-13 04:01:13 +0500

    Update settings.py

--- MHAA/settings.py
+++ MHAA/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(^b+#=!m9bu&rhj!ryc1-e-#8n$xgy@avzhtsm(uskdqz$i%#v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] 
 
 
 # Application definition
